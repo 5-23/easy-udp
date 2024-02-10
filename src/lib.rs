c5bd9e00bd712fa87ed8435131abebd2dc9c2e1c
5-23 <yhanbyeol6@gmail.com>
2024-02-10 20:42:15 +0900

    INIT

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+use std::str::from_utf8;
+
+use serde::{Deserialize, Serialize};
+
+mod server;
+
+enum Event {
+    Connect,
+    Disconnect,
+    Message(String),
+}
+
+impl Event {
+    #[allow(non_snake_case)]
+    pub fn Message<T: ?Sized + serde::Serialize>(value: &T) -> Self {
+        let binding = bincode::serialize(value).unwrap();
+        let bin = binding.as_slice();
+        Self::Message(from_utf8(bin).unwrap().to_string())
+    }
+}
